36d1bd5d82ec069f8c249b03db0bb255f128d436
demzer13 <demzer@brandeis.edu>
2016-06-30 14:49:07 -0400

    changes to quiz2

--- server/methods.js
+++
@@ -1,5 +0,0 @@
-Meteor.methods({
-  sayhiserver: function(){
-    console.log("hi!");
-  },
-})
--- server/publications.js
+++ server/publications.js
@@ -2,15 +2,3 @@
 Meteor.publish("theUserProfiles",
   function(){return UserProfiles.find();
 })
-
-/*Meteor.publish("userData", function(){
-  if(this.userId){//only if you log in can you see it
-    return Meteor.users.find({_id: this.userId},
-                            {fields: {profile:1,
-                                      "services.google.email":1}});// everyone sees everything!
-    //{fields: {'profile':1, 'things': 1}})
-    //return Meteor.users.find({_id: this.userId})
-  }else{
-    this.ready();
-  }
-});*/
